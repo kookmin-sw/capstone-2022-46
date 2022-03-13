6a695b33b2ecf7ab3ec68b21122a991321212cdd
DESKTOP-MLARTL2\wonjun <hhh9597h@naver.com>
2022-03-13 16:05:00 +0900

    캐릭터 이동제한(나중에 콜라이더로 바꿈)

--- capstone/Assets/Scripts/MovingObject.cs
+++ capstone/Assets/Scripts/MovingObject.cs
@@ -33,5 +33,12 @@ public class MovingObject : MonoBehaviour
       if(Input.GetKey(KeyCode.LeftArrow)){
         transform.Translate(Vector2.left * Speed * Time.deltaTime);
       }
+
+      //이동제한.
+      Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position); //캐릭터의 월드 좌표를 뷰포트 좌표계로 변환해준다.
+      viewPos.x = Mathf.Clamp01(viewPos.x); //x값을 0이상, 1이하로 제한한다.
+      viewPos.y = Mathf.Clamp01(viewPos.y); //y값을 0이상, 1이하로 제한한다.
+      Vector3 worldPos = Camera.main.ViewportToWorldPoint(viewPos); //다시 월드 좌표로 변환한다.
+      transform.position = worldPos; //좌표를 적용한다.
     }
 }
