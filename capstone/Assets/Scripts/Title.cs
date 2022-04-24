5cfd8b0430375276c4d7f066b85b90f845013839
DESKTOP-MLARTL2\wonjun <hhh9597h@naver.com>
2022-04-24 19:35:43 +0900

    title 제작후, 메인씬과 연결

---
+++ capstone/Assets/Scripts/Title.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public string sceneName = "MainGame";
+
+    public void ClickStart()
+    {
+        Debug.Log("로딩");
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ClickLoad()
+    {
+        Debug.Log("로드");
+    }
+
+    public void ClickExit()
+    {
+        Debug.Log("게임 종료");
+        Application.Quit();
+    }
+}
