88f5b664bd9d3146c7c82b302124aa6e52e429dc
DESKTOP-MLARTL2\wonjun <hhh9597h@naver.com>
2022-05-06 02:47:18 +0900

    쿵쾅쿵쾅 패턴

--- capstone/Assets/Scripts/boss_test/bossFoot.cs
+++ capstone/Assets/Scripts/boss_test/bossFoot.cs
@@ -6,35 +6,56 @@ public class bossFoot : MonoBehaviour
 {
     public GameObject bossFootPrefab;
     float time = 0.0f;
-    public Animator f_speed;
+    public Animator animator;
+    public Animation anim;
 
     // Update is called once per frame
 
     void Awake()
     {
-        f_speed = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        //animator.SetFloat("foot_speed", 10f);
     }
 
 
     void Update()
     {
+
       //foot_anim.SetFloat("foot_speed",4);
       time += Time.deltaTime;
 
-      if(time >= 4)
+
+      if(time >= 3)
       {
+          //animator.SetFloat("foot_speed", -3f);
+
           bossFootPrefab.SetActive(false);
           //++GameController.controller.removedGrenades;
-          Debug.Log("Destroyed");
+          Debug.Log("Destroyed_foot");
       }
 
+/*
+      if(time >= 4)
+      {
+          animator.SetFloat("foot_speed", -3f);
+
+          bossFootPrefab.SetActive(false);
+          //++GameController.controller.removedGrenades;
+          Debug.Log("Destroyed_foot");
+      }
+*/
 
     }
 
-    public void setFootSpeed(float speed)
+    public void setFootSpeed(float f_speed)
     {
+      animator.SetFloat("foot_speed", f_speed);
       //float temp = f_speed.GetFloat("foot_speed");
-      f_speed.SetFloat("foot_speed", f_speed.GetFloat("foot_speed") + speed);
+      //f_speed.SetFloat("foot_speed", f_speed.GetFloat("foot_speed") + speed);
     }
 
 
