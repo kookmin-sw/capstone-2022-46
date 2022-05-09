4288ee2520718dd29d86c5fa94e370fe84df09a9
RubyCrow <mhchoi9708@gmail.com>
2022-05-09 23:15:20 +0900

    코인(스코어 텍스트)와 연동

--- capstone/Assets/Scripts/GoldState.cs
+++ capstone/Assets/Scripts/GoldState.cs
@@ -8,14 +8,17 @@ public class GoldState : MonoBehaviour
 
   public static GoldState instance; //어디서나 접근할 수 있도록 static(정적)으로 자기 자신을 저장할 변수를 만듭니다.
   public Text scoreText; //점수를 표시하는 Text객체를 에디터에서 받아옵니다.
-  private int score=0; //점수를 관리합니다.
+  public int score=0; //점수를 관리합니다.
 
   void Awake()
     {
         if (!instance) //정적으로 자신을 체크합니다.
             instance = this; //정적으로 자신을 저장합니다.
     }
-
+    private void Update()
+    {
+        scoreText.text = "" + score;
+    }
     public void AddScore(int num) //점수를 추가해주는 함수를 만들어 줍니다.
     {
         score += num; //점수를 더해줍니다.
